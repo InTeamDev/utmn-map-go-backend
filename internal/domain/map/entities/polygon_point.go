36021740d35e570096522d26ebe75d77f64fe99c
socialrating <mdk_bol@bk.ru>
2025-05-25 17:09:39 +0500

    issues-71:create-polygon-points

--- internal/domain/map/entities/polygon_point.go
+++ internal/domain/map/entities/polygon_point.go
@@ -11,7 +11,7 @@ type PolygonPoint struct {
 }
 
 type PolygonPointRequest struct {
-	PointOrder int32   `json:"point_order" binding:"required"`
-	X          float64 `json:"x"           binding:"required"`
-	Y          float64 `json:"y"           binding:"required"`
+	PointOrder int32   `json:"point_order"`
+	X          float64 `json:"x"`
+	Y          float64 `json:"y"`
 }
